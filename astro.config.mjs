1b5809bdd89f1f18c6642d0aea2c188460ce4f52
alexisabels <isabelsanchezalex@gmail.com>
2024-07-07 00:41:44 +0200

    tailwind installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server", // Cambia a 'server' para el modo servidor o 'hybrid' para un enfoque mixto
+  output: "server",
 });
